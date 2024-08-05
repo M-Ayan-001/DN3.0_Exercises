9e9a7c6bbd5be02d8302597bf02168880377eac4
M-Ayan-001 <amukherjee996@gmail.com>
2024-08-05 19:53:05 +0530

    Week 2 changes done

---
+++ Week_2_Exercises/spring_core_maven/Ex-4-Creating and Configuring a Maven Project/EX_4_Library_Management/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,36 @@
+package com.library.repository;
+
+public class BookRepository {
+	private String bookID;
+	private String bookName;
+	private int bookPrice;
+
+	public String getBookID() {
+		return bookID;
+	}
+
+	public void setBookID(String bookID) {
+		this.bookID = bookID;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public int getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "BookRepository [bookID=" + bookID + ", bookName=" + bookName + ", bookPrice=" + bookPrice + "]";
+	}
+}
